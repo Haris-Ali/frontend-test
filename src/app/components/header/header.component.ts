483782307a2176da18f34655e39bedb6e5441c55
Haris-Ali <haris.ali@vizteck.com>
2023-02-25 11:23:58 +0500

    Header complete

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeaderComponent {
+  isMobileMenuOpen: boolean = false;
 
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
